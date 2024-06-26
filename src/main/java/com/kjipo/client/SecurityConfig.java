a309d79da40e10cfeb3cf711e7f1a004586e5fea
Tore Brede <tore.brede@gmail.com>
2024-06-26 20:52:14 +0200

    Starting on Canvas OAuth2 login example

---
+++ src/main/java/com/kjipo/client/SecurityConfig.java
@@ -0,0 +1,91 @@
+package com.kjipo.client;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final String authorizationUri;
+    private final String tokenUri;
+    private final String clientId;
+    private final String clientKey;
+    private final String redirectUri;
+
+
+    public SecurityConfig(@Value("${authorization-uri}") String authorizationUri,
+                          @Value("${client-id}") String clientId,
+                          @Value("${client-key}") String clientKey,
+                          @Value("${token-uri}") String tokenUri,
+                          @Value("${redirect-uri}") String redirectUri) {
+        this.authorizationUri = authorizationUri;
+        this.tokenUri = tokenUri;
+        this.clientId = clientId;
+        this.clientKey = clientKey;
+        this.redirectUri = redirectUri;
+    }
+
+    @Order(2)
+    @Bean
+    public SecurityFilterChain loginEndpoint(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests((requests) ->
+                        requests.requestMatchers("/login")
+                                .permitAll())
+                .build();
+    }
+
+    @Order(1)
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests((requests) ->
+                        requests.anyRequest()
+                                .authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(clientRegistration());
+    }
+
+    /**
+     * https://docs.spring.io/spring-security/site/docs/5.2.12.RELEASE/reference/html/oauth2.html#oauth2login-register-clientregistrationrepository-bean
+     *
+     * @return
+     */
+    private ClientRegistration clientRegistration() {
+        return ClientRegistration.withRegistrationId("canvas")
+                .clientId(clientId)
+                .clientSecret(clientKey)
+                // TODO Is this the correct grant type?
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationUri(authorizationUri)
+                .tokenUri(tokenUri)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .redirectUri(redirectUri)
+                .build();
+    }
+
+
+//    @Bean
+//    public OAuth2AuthorizedClientService authorizedClientService() {
+//        return new InMemoryOAuth2AuthorizedClientService(
+//                clientRegistrationRepository());
+//    }
+
+
+}
